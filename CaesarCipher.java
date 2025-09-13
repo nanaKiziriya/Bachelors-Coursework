5321629a54e4ac831430c010e098992efd0802a0
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2025-09-13 17:09:15 -0400

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -7,25 +7,29 @@ public class CaesarCipher{
     static int snippetLength = 50;
     
     public static void main(String[]args){
-        System.out.println("Welcome to Caesar Cipher.\n");
-        System.out.println("Encrypt? Y/N");
+        System.out.println("Welcome to Caesar Cipher.");
+        System.out.println("\nEncrypt? Y/N");
+        System.out.print("> ");
         if(sc.next().toUpperCase().charAt(0)=='Y') runEncryption(true);
-        System.out.println("Decrypt?");
+        System.out.println("\nDecrypt?");
+        System.out.print("> ");
         if(sc.next().toUpperCase().charAt(0)=='Y') runEncryption(false);
-        System.out.println("Program terminate.");
+        System.out.println("\nProgram terminate.");
     }
 
     static void runEncryption(boolean encrypt){
-        System.out.printf("Begin entering %smessage. Enter line \"%s\" to end message.\n",
+        System.out.printf("\nBegin entering %smessage. Enter line \"%s\" to end message.\n",
                            (encrypt)?"":"encoded ",exitMsg);
         
         StringBuilder unfilteredSB = new StringBuilder();
         
-        String token = sc.next();
+        String line = sc.nextLine();
         do{
-            unfilteredSB.append(token);
-            token = sc.next();
-        } while(!token.equalsIgnoreCase(exitMsg));
+            Scanner token = new Scanner(line);
+            while(token.hasNext()) unfilteredSB.append(token.next());
+            System.out.print("> ");
+            line = sc.nextLine();
+        } while(!line.equalsIgnoreCase(exitMsg));
         
         char[] unfilteredChars;
         StringBuilder filteredSB = new StringBuilder();
@@ -40,7 +44,7 @@ public class CaesarCipher{
         
         String filteredStr = filteredSB.toString(); // alphabetical, of proper case
         
-        System.out.println("Received. ");
+        System.out.println("\nReceived. ");
         
         if(encrypt){
             System.out.println("Enter number for cipher shift.");
@@ -49,28 +53,28 @@ public class CaesarCipher{
             for(int i=1;i<26;i++){
                 // when decrypting, shift DOWN
                 System.out.printf("%-2d ",i);
-                decryptShift(filteredStr, i, snippetLength);
+                decryptShift(filteredStr, i, Math.min(snippetLength,filteredStr.length()));
                 System.out.println();
             }
         }
-            
+        
+        System.out.print("> ");
         int cipher = Math.abs(sc.nextInt()); // assert non-negative
 
         if(encrypt){
-            System.out.println("Cipher selected. Encrypted message:");
+            System.out.println("\nCipher selected. Encrypted message:");
             encryptShift(filteredStr, cipher);
         } else {
-            System.out.println("Number selected is cipher. Decrypted message:");
+            System.out.println("\nNumber selected is cipher. Decrypted message:");
             decryptShift(filteredStr, cipher, filteredStr.length());
         }
-        System.out.println();
         System.out.println();
         
     }
 
     private static void encryptShift(String filteredStr, int cipher){
         for(int j=0; j<filteredStr.length(); j++){
-            char c = (char)(string.charAt(j)+cipher);
+            char c = (char)(filteredStr.charAt(j)+cipher);
             c -= (c>90)? 26:0;
             System.out.print(c);
             if(j%5==4) System.out.print(" ");
@@ -79,7 +83,7 @@ public class CaesarCipher{
 
     private static void decryptShift(String filteredStr, int cipher, int length){
         for(int j=0; j<length; j++){
-                char c = (char)(string.charAt(j)-cipher);
+                char c = (char)(filteredStr.charAt(j)-cipher);
                 c += (c<97)? 26:0;
                 System.out.print(c);
             }
