0084b28ac379c07a7234dd6e5b79997e3f2c7468
nanaKiziriya <144845521+nanaKiziriya@users.noreply.github.com>
2025-12-09 17:40:06 -0500

    Update HammingCode.java

--- HammingCode.java
+++ HammingCode.java
@@ -1,15 +1,17 @@
 public class HammingCode{
 
-  private final static byte systemParity = 1; // Either even (0) or odd (10. Standard is odd (1), meaning xor all bit values results in all 1's
-  private final static int numDataBits = Math.pow(2,numParityBits)-numParityBits-1;
-  private final static int numParityBits = 3;
-  private final static int[] bitValues;
+  private static byte systemParity = 1; // Either even (0) or odd (10. Standard is odd (1), meaning xor all bit values results in all 1's
+  private static int numDataBits = 4; // Math.pow(2,numParityBits)-numParityBits-1
+  private static int numParityBits = 3; // parity bits are powers of 2, starting from 1
+  private static int[] bitValues = {3,5,6,7,1,2,4};
   private final static Scanner sc = new Scanner(System.in);
 
   // DONE
   public static void main(String[] args){
-    System.out.printf("WELCOME TO HAMMING CODE PROGRAM. Current status: Hamming(%d,%d)\n\n",numDataBits+numParityBits,numParityBits);
-    System.out.println("Note: Transmissions are sent as a combination of data bits and parity bits, for error correction purposes.\n");
+    System.out.println("WELCOME TO HAMMING CODE PROGRAM.");
+    System.out.println("Note: Transmissions are sent as a combination of data bits and parity bits for error correction purposes.\n");
+
+    printSystemStatus();
     
     while(true);
       int input = inputOptions();
@@ -23,17 +25,17 @@ public class HammingCode{
 
   // DONE
   // Underlies all user inputs
-  static String userInput(){
+  private static String userInput(){
     System.out.print(" > ");
     return sc.nextLine();
   }
 
   // DONE
   // Choosing btwn options 0,1,2,3
-  static int inputOptions(){
+  private static int inputOptions(){
     System.out.println("What would you like to do?\n"
-                      + "0 - terminate program\n"
-                      + "1 - change Hamming system\n"
+                      + "0 - Terminate program\n"
+                      + "1 - Change Hamming Code system\n"
                       + "2 - Tx: transmit a message\n"
                       + "3 - Rx: receive and correct a message\n");
     while(true){
@@ -46,6 +48,25 @@ public class HammingCode{
         System.out.println("Input must be an integer. Try again.");
       }
     }
+  }
+
+  private static void setSystemStatus(){
+    
+  }
+
+  private static void printSystemStatus(){
+    System.out.printf("CURRENT STATUS\n"
+                      + "System parity: %s\n"
+                      + "# of parity bits: %d\n"
+                      + "# of data bits: %d\n"
+                      + "Total Tx length: %d\n"
+                      + "Value of each bit: %s\n",
+                      systemParity==1?"ODD":"EVEN",numParityBits,numDataBits,numParityBits+numDataBits,bitValues);
+
+    
+    // Math.pow(2,numParityBits)-numParityBits-1
+     = 3; // parity bits are powers of 2, starting from 1
+     = {3,5,6,7,1,2,4};
   }
 
   static uiBinTup
