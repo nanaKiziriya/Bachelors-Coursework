df6f85b99fd3084285ec2380a05f987609a07081
Nana <144845521+nanaKiziriya@users.noreply.github.com>
2026-09-22 13:18:53 -0400

    Create MyHashMap.java

---
+++ CISC3220_AnalysisOfAlgorithms/MyHashMap.java
@@ -0,0 +1,29 @@
+/*  TASK:
+    Create your own HashMap implementation. Do not use a built-in HashMap or similar map structure.
+    
+    Requirements:
+    
+        Store key-value pairs.
+        Use an array of "buckets" - if you're doing this in Java, the buckets will probably be easiest as Lists.
+        Use a hash function and the mod operation to determine which bucket a key belongs in.
+        Handle collisions by allowing multiple entries in the same bucket.
+        Implement methods to:
+            add or update a key-value pair
+            remove a key-value pair
+            check whether a key exists
+        Print enough output to show that each operation works correctly.
+*/
+
+import java.util.ArrayList; // jav
+import java.util.Map; // interface, no implementation :)
+  
+public static void main(String[] args){
+  // for testing
+  MyHashMap = new MyHashMap();
+}
+
+public class MyHashMap implements Map{
+  MyHashMap(){
+  }
+  MyHashMap(){
+}
