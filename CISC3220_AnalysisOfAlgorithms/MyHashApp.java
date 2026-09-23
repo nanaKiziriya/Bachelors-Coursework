5b74e3e4f7c1b96c6d5a05bc7d51020ebabb45a2
Nana <144845521+nanaKiziriya@users.noreply.github.com>
2026-09-23 14:48:28 -0400

    Update MyHashApp.java

--- CISC3220_AnalysisOfAlgorithms/MyHashApp.java
+++ CISC3220_AnalysisOfAlgorithms/MyHashApp.java
@@ -14,13 +14,16 @@
         Print enough output to show that each operation works correctly.
 */
 
+
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.Objects;
 
+// Nana Kiziriya's MyHashMap assignment... I went overboard. It's unironically a mental illness.
+
 public class MyHashApp {
     public static void main(String[]args) {
-        System.out.println("Nana Kiziriya's MyHashMap assignment... I went overboard. It's unironically a mental illness.")
+        
         MyHashMap<String,Integer> hm = new MyHashMap<>();
         
         System.out.println("\nAdding entries to MyHashMap in this order:");
@@ -89,7 +92,11 @@ public class MyHashMap<K,V> {
 
         buckets = (KeyValuePair<K,V>[]) new KeyValuePair[initCapacity];
         this.loadFactor = loadFactor;
-        this.hashFactor = generatehashFactor(initCapacity);
+        updateHashFactor();
+    }
+
+    private void updateHashFactor(){
+        this.hashFactor = generatehashFactor(this.buckets.length);
     }
 
     // adds kvpair (updates if k exists)
@@ -124,10 +131,7 @@ public class MyHashMap<K,V> {
         KeyValuePair<K,V> current = buckets[index];
 
         while (current != null) {
-            if (Objects.equals(current.key, key)) {
-                return current.value;
-            }
-
+            if (current.key.equals(key)) return current.value;
             current = current.next;
         }
 
@@ -139,10 +143,7 @@ public class MyHashMap<K,V> {
         KeyValuePair<K,V> current = buckets[index];
 
         while (current != null) {
-            if (Objects.equals(current.key, key)) {
-                return true;
-            }
-
+            if (current.key.equals(key)) return true;
             current = current.next;
         }
 
@@ -151,25 +152,19 @@ public class MyHashMap<K,V> {
 
     public V remove(K key) {
         int index = calculateHashedIndex(key);
-        // KeyValuePair<K,V> current = buckets[index];
-        // KeyValuePair<K,V> previous = null;
-
+        KeyValuePair<K,V> current = buckets[index], previous=null;
+    
         while (current != null) {
-            if (Objects.equals(current.key, key)) {
-                if (previous == null) {
-                    buckets[index] = current.next;
-                } else {
-                    previous.next = current.next;
-                }
-
+            if (current.key.equals(key)) {
+                if (previous == null) buckets[index] = current.next;
+                else previous.next = current.next;
                 size--;
                 return current.value;
             }
-
             previous = current;
             current = current.next;
         }
-
+        
         return null;
     }
 
@@ -190,32 +185,45 @@ public class MyHashMap<K,V> {
             index+=b; index%=capacity;
             index*=hashFactor; index%=capacity;
         }
-        return index;
+        return index + index<0? capacity:0;
     }
 
+    // returns random int that's coprime to newCapacity
     private static int generatehashFactor(int newCapacity){
         Random random = new Random();
         int newhashFactor = random.nextInt()%newCapacity;
         while(!coprime(newCapacity,newhashFactor)) newhashFactor = random.nextInt()%newCapacity;
-        return newhashFactor;
+        return newhashFactor%newCapacity;
+    }
+
+    // uses Euclidean Algorithm
+    private static boolean coprime(int M, int m){
+        if(M==0||m==0) return false; // all ints are factors of 0
+        if(M<0) M*=-1;
+        if(m<0) m*=-1;
+        if(M<m){ int hold=M; M=m; m=hold; }
+        
+        while(M%m>0){ int hold = M%m; M=m; m=hold; } // M>m
+        return m==1;
     }
 
     @SuppressWarnings("unchecked")
     private void resize() {
         KeyValuePair<K,V>[] oldBuckets = buckets;
-        buckets = (KeyValuePair<K,V>[]) new KeyValuePair[oldBuckets.length * 2];
+        buckets = (KeyValuePair<K,V>[]) new KeyValuePair[oldBuckets.length * 2]; // double da capacityyy
+        updateHashFactor();
 
-        for (KeyValuePair<K,V> bucket : oldBuckets) {
-            KeyValuePair<K,V> current = bucket;
+        for (KeyValuePair<K,V> oldBucket : oldBuckets){
+            KeyValuePair<K,V> current = oldBucket; // start at head of this bucket chain
 
-            while (current != null) {
-                KeyValuePair<K,V> next = current.next;
+            while(current != null) {
+                // KeyValuePair<K,V> next = current.next;
                 int newIndex = calculateHashedIndex(current.key);
+                this.put(current.key,current.value);
+                // current.next = buckets[newIndex];
+                // buckets[newIndex] = current;
 
-                current.next = buckets[newIndex];
-                buckets[newIndex] = current;
-
-                current = next;
+                current = current.next;
             }
         }
     }
@@ -223,10 +231,15 @@ public class MyHashMap<K,V> {
     public String toString(){
         StringBuilder sb = new StringBuilder();
         sb.append("{");
-        for(ArrayList arr:this.entries) if(arr!=null) for(E entry:arr){
-            sb.append(entry.toString());
-            sb.append(", ");
+
+        for(KeyValuePair<K,V> bucket:buckets){
+            KeyValuePair<K,V> current = bucket;
+            while(current!=null){
+                sb.append(current.toString());
+                sb.append(", ");
+            }
         }
+        
         sb.replace(sb.length()-2,sb.length(),"}");
         return sb.toString();
     }
@@ -234,155 +247,11 @@ public class MyHashMap<K,V> {
     private static class KeyValuePair<K,V> {
         private final K key; // do not change key! else wrong bucket!
         private V value;
-        // private KeyValuePair<K,V> next;
+        private KeyValuePair<K,V> next; // way better than removing from an ArrayList (O(n))
 
         KeyValuePair(K key, V value) {
             this.key = key;
             this.value = value;
         }
-    }
-}
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////
-
-public class MyHashMap<K,V>{
-    
-    private MyHashTable<KeyValuePair<K,V>> entries; // K,V pairs
-    private MyHashTable<K> keys;   // automatic growth, constant-time storage and retrieval
-    private MyHashTable<V> values; // automatic growth, constant-time storage and retrieval
-
-    private class MyHashTable<K,V>{
-        
-        /* Datafields */
-        
-        private KeyValuePairList<K,V> entries[]; // I do not want automatic container growth: Do not use ArrayList, Vector, etc. for external container
-        private int numEntries = 0; // number of entries in all ArrayLists; NOT necessarily equal to loadSize (num ArrayLists)
-        private int loadSize = 0; // entries.length return CAPACITY, not number of ArrayLists in entries
-        private double loadFactor;
-        private int growthFactor; // capacity grows by growthFactor when loadSize exceeds loadFactor*capacity
-        private int hashFactor; // Always < capacity; used for hash calculation; value not hardcoded; generated randomly to be coprime to container capacity: see this.generatehashFactor()
-    
-        /* Constructors */
-        
-        MyHashTable(){
-            this(16); } // Java HashSet default
-        MyHashTable(int initCapacity){
-            this(initCapacity, 0.75); } // Java HashSet default
-        MyHashTable(int initCapacity, double loadFactor){
-            this(initCapacity, loadFactor, 2); } // Java HashSet default
-        @SuppressWarnings("unchecked")
-        MyHashTable(int initCapacity, double loadFactor, int growthFactor){
-            this.entries = (KeyValuePairList<KeyValuePair<K,V>>[]) new KeyValuePairList<?>[initCapacity];
-            this.loadFactor = loadFactor;
-            this.growthFactor = growthFactor;
-            this.hashFactor = generatehashFactor(initCapacity);
-        }
-    
-        public boolean put(K key, V value){
-            int hashIndex = calculateHashedIndex(key);
-            if(entries[hashIndex]==null) entries[hashIndex] = new ArrayList<KeyValuePair<K,V>>(2);
-            if(this.contains(KeyValuePair)) return false; // checks entire ArrayList at proper hash index
-            if(this.containsKey(KeyValuePair.getKey()){
-                this.remove(KeyValuePair.key)entries[hashIndex].setValue()
-            entries[hashIndex].add(o);
-            numEntries++;
-            return true;
-        }
-        public boolean contains(Object o){
-            int hashIndex = calculateHashedIndex(o);
-            return entries[hashIndex]!=null && entries[hashIndex].contains(o);
-        }
-        public boolean remove(Object o){
-            int hashIndex = calculateHashedIndex(o);        
-            if(! entries[hashIndex].contains(o)) return false; // checks entire ArrayList at proper hash index
-            entries[hashIndex].remove(o);
-            numEntries--;
-            if(entries[hashIndex].isEmpty()) entries[hashIndex] = null;
-            return true;
-        }
-        public int size(){ return this.numEntries; }
-    
-        public String toString(){
-            StringBuilder sb = new StringBuilder();
-            sb.append("{");
-            for(ArrayList arr:this.entries) if(arr!=null) for(E KeyValuePair:arr){
-                sb.append(KeyValuePair.toString());
-                sb.append(", ");
-            }
-            sb.replace(sb.length()-2,sb.length(),"}");
-            return sb.toString();
-        }
-    
-        /* Private Methods */
-        
-        private int calculateHashedIndex(Object o){ return calculateHashedIndex(0,this.hashFactor,this.entries.length); }
-        
-        // Equivalent objects must return the same index
-        // Easier alternative: just use hashCode()
-        private static int calculateHashedIndex(Object o,int hashFactor, int capacity){
-            byte bytes[] = (""+o.hashCode()).getBytes();
-            int index = 0;
-            for(byte b : bytes){
-                index+=b; index%=capacity;
-                index*=hashFactor; index%=capacity;
-            }
-            return index;
-        }
-    
-        // returns random int coprime to newCapacity
-        private static int generatehashFactor(int newCapacity){
-            Random random = new Random();
-            int newhashFactor = random.nextInt()%newCapacity;
-            while(!coprime(newCapacity,newhashFactor)) newhashFactor = random.nextInt()%newCapacity;
-            return newhashFactor;
-        }
-    
-        // uses Euclidean Algorithm
-        private static boolean coprime(int M, int m){
-            if(M==0||m==0) return false; // all ints are factors of 0
-            if(M<0) M*=-1;
-            if(m<0) m*=-1;
-            if(M<m){ int hold=M; M=m; m=hold; }
-            
-            while(M%m>0){ int hold = M%m; M=m; m=hold; } // M>m
-            return m==1;
-        }
-    }
-
-    private class KeyValuePairList<K,V>{
-        private ArrayList<KeyValuePair<K,V>> list;
-        public addKeyValuePair(K, k, V v){
-            if(this.containsKey(k)) list.replace(this.indexOfKey(k),new KeyValuePair(k,v));
-            else list.add(new KeyValuePair(k,v));
-        }
-        public void removeKey(K k){
-            if(containsKey(k)) list.remove(indexOfKey(k));
-        }
-        private boolean containsKey(K k){
-            return indexOfKey(k)!=-1;
-        }
-        private int indexOfKey(K k){
-            for(int i=0; i<list.size; i++) if(list.get(i).equals(k)) return i;
-            return -1;
-        }
-    }
-    
-    private class KeyValuePair<K,V>{
-        K key;
-        V value;
-        KeyValuePair(K key, V value){
-            this.key = key;
-            this.value = value;
-        }
-        K getKey(){ return key; }
-        V getValue(){ return value; }
-        void setValue(V value){ this.value = value; }
-        
     }
 }
