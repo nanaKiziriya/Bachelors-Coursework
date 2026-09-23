9c07f4602eacbc2a981b7906f0957011b2d9c1cd
Nana <144845521+nanaKiziriya@users.noreply.github.com>
2026-09-23 03:14:04 -0400

    Update MyHashMap.java

--- CISC3220_AnalysisOfAlgorithms/MyHashMap.java
+++ CISC3220_AnalysisOfAlgorithms/MyHashMap.java
@@ -14,16 +14,83 @@
         Print enough output to show that each operation works correctly.
 */
 
-import java.util.ArrayList; // jav
-import java.util.Map; // interface, no implementation :)
-  
-public static void main(String[] args){
-  // for testing
-  MyHashMap = new MyHashMap();
+import java.util.Random;
+
+public class MyHashApp{
+    public static void main(String[] args){
+        // for testing
+        MyHashMap = new MyHashMap();
+    }
 }
 
-public class MyHashMap implements Map{
-  MyHashMap(){
-  }
-  MyHashMap(){
+// Hashing: equivalent Objects must return the same hash, with different Objects having hashes that (almost) never coincide.
+class MyHashSet<E>{
+    private E entries[]; // Do NOT want automatic container growth: Do not use ArrayList, Vector, etc.
+    private int hashFactor; // For hash calculation; not hardcoded; generated randomly to be coprime to container capacity: see this.generateHashFactor()
+    private double loadFactor, growthFactor; // capacity grows by growthFactor when size exceeds loadFactor*capacity
+    private static Random random = new Random(); // for generateHashFactor()
+    
+    MyHashSet(){
+        this(16); }
+    MyHashSet(int initCapacity){
+        this(initCapacity, 0.75); }
+    MyHashSet(int initCapacity, double loadFactor){
+        this(initCapacity, loadFactor, 1.5); }
+    MyHashSet(int initCapacity, double loadFactor, double growthFactor){
+        this.values = new V[initCapacity];
+        this.loadFactor = loadFactor;
+        this.growthFactor = growthFactor;
+        this.hashFactor = generateHashFactor(initCapacity);
+    }
+
+    public boolean add(Object o)
+    public boolean contains(Object o)
+    public boolean remove(Object o)
+    public int size()
+
+    
+    /* Private Helper Methods */
+
+    // returns random int coprime to newCapacity
+    private static int generateHashFactor(int newCapacity){
+        int newHashFactor = random.nextInt();
+        while(!coprime(newHashFactor,newCapacity)) newHashFactor = random.nextInt();
+        return newHashFactor;
+    }
+
+    // uses Euclidean Algorithm
+    private static boolean coprime(int M, int m){
+        if(M==0||m==0) return false; // all ints are factors of 0
+        if(M<0) M*=-1;
+        if(m<0) m*=-1;
+        if(M<m){ int hold=M; M=m; m=hold; }
+        
+        while(M%m>0){ int hold = M%m; M=m; m=hold; } // M>m
+        return m==1;
+    }
+}
+
+
+//INC
+class MyHashMap<K,V> {
+
+    
+    private MyHashSet<K> keys;   // automatic growth, constant-time storage and retrieval
+    private MyHashSet<V> values; // automatic growth, constant-time storage and retrieval
+    
+
+    // Similar constructors as Java HashMap
+    // Same default load/growth factors as ArrayList
+    MyHashMap(){
+        this(16); }
+    MyHashMap(int initCapacity){
+        this(initCapacity, 0.75); }
+    MyHashMap(int initCapacity, double loadFactor){
+        this(initCapacity, loadFactor, 1.5); }
+    MyHashMap(int initCapacity, double loadFactor, double growthFactor){
+        this.values = new V[initCapacity];
+        this.loadFactor = loadFactor;
+        this.growthFactor = growthFactor;
+        this.hashFactor = newHashFactor(initCapacity);
+    }
 }
